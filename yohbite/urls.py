69f2597b9b36b051596c7733a19b9759a74074e9
csilva <csilva@silvatech.org>
2018-07-10 22:17:19 -0600

    Added sign in functionality with sign out and sign up. Added templates along with addition views and models for user and restaurants. Urls were also facilitated for those changes. Migration for pillow and models conducted

--- yohbite/urls.py
+++ yohbite/urls.py
@@ -17,8 +17,21 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from yohbiteapp import views
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', views.home, name='home')
-]
+    url(r'^$', views.home, name='home'),
+    url(r'^restaurant/sign-in/$', auth_views.login,
+        {'template_name': 'restaurant/sign_in.html'},
+        name='restaurant-sign-in'),
+    url(r'^restaurant/sign-up/$', views.restaurant_sign_up,
+        name='restaurant-sign-up'),
+    url(r'^restaurant/sign-out/$', auth_views.logout,
+        {'next_page': '/'},
+        name='restaurant-sign-out'),
+    url(r'^restaurant/$', views.restaurant_home, name='restaurant-home')
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- yohbiteapp/admin.py
+++ yohbiteapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from yohbiteapp.models import Restaurant
 # Register your models here.
+
+
+admin.site.register(Restaurant)

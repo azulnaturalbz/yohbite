9a5e7fdabd52d075cfb4127dedf6f135cf73cceb
csilva <csilva@silvatech.org>
2018-07-15 11:03:20 -0600

    Added meal, order and order details models. Complemented with userform and meal forms in the forms.py. Views were added for account registration and editing. Viewing, adding and editing meals. Viewing and resolving orders. Also models view were appended to admin.py. All respective templates were added for views . Urls and Styles were added.

--- yohbiteapp/admin.py
+++ yohbiteapp/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from yohbiteapp.models import Restaurant, Customer, Driver
+from yohbiteapp.models import Restaurant, Customer, Driver, Meal, Order,OrderDetails
 # Register your models here.
 
 
 admin.site.register(Restaurant)
 admin.site.register(Customer)
 admin.site.register(Driver)
+admin.site.register(Meal)
+admin.site.register(Order)
+admin.site.register(OrderDetails)
+

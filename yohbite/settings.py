f5d0a20330a01c242114aacfc9129cc8d5546287
csilva <csilva@silvatech.org>
2018-07-19 16:35:45 -0600

    Updated settings file to work with postgres db.

--- yohbite/settings.py
+++ yohbite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import credentials
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -81,22 +82,22 @@ WSGI_APPLICATION = 'yohbite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': credentials.dbname,
-#         'USER': credentials.dbuser,
-#         'PASSWORD': credentials.dbpassword,
-#         'HOST': credentials.dbhome,
-#         'PORT': credentials.dbport,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': credentials.dbname,
+        'USER': credentials.dbuser,
+        'PASSWORD': credentials.dbpassword,
+        'HOST': credentials.dbhome,
+        'PORT': credentials.dbport,
+    }
+}
 
 
 # Password validation

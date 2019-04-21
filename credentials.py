bd42f420513a31817ea0daf9e534e7cda3252e19
Cristian Silva <csilva@silvatech.org>
2019-04-21 17:24:15 -0600

    remove python.dotenv and updated requirements.txt

--- credentials.py
+++ credentials.py
@@ -1,11 +1,5 @@
 import os
 # settings.py
-from dotenv import load_dotenv
-load_dotenv()
-
-from pathlib import Path  # python3 only
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
 
 DBUSER = os.environ['YBDBUSER']
 DBPASSWORD = os.environ['YBDB_PASSWORD']
